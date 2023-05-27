0b52f3da34ecb96730b9722e6b0373df0fe51869
Riaz Hassan <rhassan70@gmail.com>
2023-05-27 22:52:13 +0500

    commit message

---
+++ rest-api/items/create.php
@@ -0,0 +1,23 @@
+<?php
+function create(){
+		
+	$stmt = $this->conn->prepare("
+		INSERT INTO ".$this->itemsTable."(`name`, `description`, `price`, `category_id`, `created`)
+		VALUES(?,?,?,?,?)");
+	
+	$this->name = htmlspecialchars(strip_tags($this->name));
+	$this->description = htmlspecialchars(strip_tags($this->description));
+	$this->price = htmlspecialchars(strip_tags($this->price));
+	$this->category_id = htmlspecialchars(strip_tags($this->category_id));
+	$this->created = htmlspecialchars(strip_tags($this->created));
+	
+	
+	$stmt->bind_param("ssiis", $this->name, $this->description, $this->price, $this->category_id, $this->created);
+	
+	if($stmt->execute()){
+		return true;
+	}
+ 
+	return false;		 
+}
+?>
---
+++ rest-api/items/read1.php
@@ -0,0 +1,11 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+echo "b4 inclue ";
+//include_once 'database.php';
+// include_once 'items.php';
+include_once '../class/items.php';
+echo "after inclue 2 ";
+
+?>
